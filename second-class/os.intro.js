067868287d81952f733fe3d15b3381fb196c2c45
tdadadavid <davidtofunmidada@gmail.com>
2023-03-22 14:01:09 +0100

    more on operating system

--- second-class/os.intro.js
+++ second-class/os.intro.js
@@ -1,4 +1,31 @@
+// Working with Operating System.
+
+
 const os = require('os');
 
+// getting the hostname
 const host = os.hostname()
+console.log('Hostname')
 console.log(host);
+
+console.log();
+
+// check the free memory on the computer
+const freeMemory = os.freemem()
+console.log(`I have ${freeMemory} free memory in my computer`);
+
+console.log();
+
+// print out the home directory
+const homeDirectory = os.homedir()
+console.log(`My home directory is ${homeDirectory}`)
+
+console.log();
+
+const computerCpus = os.cpus();
+console.log(computerCpus);
+
+console.log()
+
+const computerArchi = os.arch();
+console.log(`My computer architcture is a ${computerArchi} system`);
