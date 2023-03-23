791653975b18df52e2bf36cefabdd102c28d6af8
tdadadavid <davidtofunmidada@gmail.com>
2023-03-23 13:02:53 +0100

    work on solution to assignment

--- second-class/fs.intro.js
+++ second-class/fs.intro.js
@@ -3,12 +3,20 @@
 const fs = require("fs");
 
 // create folder
-fs.mkdirSync('nodejs-class-file-system', function(err){
+// fs.mkdir('nodejs-class-file-system', function(err){
+//     if(err){
+//        console.log(`Error creating directory ${err}`) 
+//     }else{
+//         console.log('directory created')
+//     }e
+// })
+
+
+// creating a file in the folder
+fs.appendFile('nodejs-class-file-system/solution-to-assignment',"This is the assignment solution","base64url", function(err){
     if(err){
-       console.log(`Error creating directory ${err}`) 
+        console.log(err)
     }else{
-        console.log('directory created')
+        console.log("File created and written into");
     }
 })
-
-// use 
